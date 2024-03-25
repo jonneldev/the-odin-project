28299f103d63c486cbb41f60316ea46c0f49215b
jonneldev <jonneldosado.code@gmail.com>
2024-03-25 11:36:11 +0800

    rock-paper-scissor added function playGame

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -6,32 +6,38 @@ function getComputerChoice() {
     return choices[randomChoice];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playGame() {
+    function playRound(playerSelection, computerSelection) {
 
-    let playerScore = 0;
-    let computerScore = 0;
+        let playerScore = 0;
+        let computerScore = 0;
+        
+        //Player select
+        playerSelection = prompt('Please choose..').toLowerCase();
     
-    //Player select
-    playerSelection = prompt('Please choose..').toLowerCase();
+        //Computer select
+        computerSelection = getComputerChoice();
+    
+        //Logic to determine the winner
+        if (playerSelection === computerSelection) {
+            return `It's a tie! your opponent also choose ${computerSelection}`;
+        } else if (
+            (playerSelection === 'rock' && computerSelection === 'scissor') ||
+            (playerSelection === 'paper' && computerSelection === 'rock') ||
+            (playerSelection === 'scissor' && computerSelection ===
+            'paper')
+        ) {
+            playerScore+=1
+            return `You win! ${playerSelection} beats ${computerSelection}.`;
+        } else {
+            computerScore+=1
+            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    
+        }
+    }
 
-    //Computer select
-    computerSelection = getComputerChoice();
-
-    //Logic to determine the winner
-    if (playerSelection === computerSelection) {
-        return `It's a tie! your opponent also choose ${computerSelection}`;
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissor' && computerSelection ===
-        'paper')
-    ) {
-        playerScore++
-        return `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else {
-        computerScore++
-        return `You lose! ${computerSelection} beats ${playerSelection}.`;
-
+    for (let round = 0; round < 5; round++) {
+        return playRound();
     }
 
 }
