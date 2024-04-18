3440968552f81fda3dfa56801caa9efe2619e9a5
jonneldev <jonneldosado.code@gmail.com>
2024-04-18 20:10:07 +0800

    update

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,49 +1,38 @@
-
 const choices = ['rock', 'paper', 'scissor'];
 
 function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() *3);
+    const randomChoice = Math.floor(Math.random() * 3);
     return choices[randomChoice];
 }
 
+function playRound(playerSelection, computerSelection) {
+
+    //Player select
+    playerSelection = prompt('Please choose..').toLowerCase();
+
+    //Computer select
+    computerSelection = getComputerChoice();
+
+    //Logic to determine the winner
+    if (playerSelection === computerSelection) {
+        console.log(`It's a tie! your opponent also choose ${computerSelection}`);
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper')
+    ) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+    }
+}
+
 function playGame() {
-    function playRound(playerSelection, computerSelection) {
-
-        let playerScore = 0;
-        let computerScore = 0;
-        
-        //Player select
-        playerSelection = prompt('Please choose..').toLowerCase();
-    
-        //Computer select
-        computerSelection = getComputerChoice();
-    
-        //Logic to determine the winner
-        if (playerSelection === computerSelection) {
-            return `It's a tie! your opponent also choose ${computerSelection}`;
-        } else if (
-            (playerSelection === 'rock' && computerSelection === 'scissor') ||
-            (playerSelection === 'paper' && computerSelection === 'rock') ||
-            (playerSelection === 'scissor' && computerSelection ===
-            'paper')
-        ) {
-            playerScore+=1
-            return `You win! ${playerSelection} beats ${computerSelection}.`;
-        } else {
-            computerScore+=1
-            return `You lose! ${computerSelection} beats ${playerSelection}.`;
-    
-        }
-    }
-
-    for (let round = 0; round < 5; round++) {
-        return playRound();
-    }
-
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
 }
 
 
-
-
-  
-  
