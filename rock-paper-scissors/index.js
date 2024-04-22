364d57b133f99f1cd883e6193388f4efe9ac6715
jonneldev <jonneldosado.code@gmail.com>
2024-04-22 12:43:53 +0800

    dom manipulation

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,38 +1,49 @@
 const choices = ['rock', 'paper', 'scissor'];
 
 function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * 3);
+    const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
 
-function playRound(playerSelection, computerSelection) {
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorButton = document.querySelector("#scissorButton");
+const div = document.querySelector("div");
+const spanVs = document.querySelector("#spanVs");
 
-    //Player select
-    playerSelection = prompt('Please choose..').toLowerCase();
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    spanC.textContent = computerSelection;
+    div.appendChild(spanC)
 
-    //Computer select
-    computerSelection = getComputerChoice();
+
 
     //Logic to determine the winner
     if (playerSelection === computerSelection) {
-        console.log(`It's a tie! your opponent also choose ${computerSelection}`);
+        console.log(`It's a tie! Your opponent also chose ${computerSelection}`);
+
+        span.textContent = playerSelection;
+        div.insertBefore(span, spanVs)
+        
+        
     } else if (
         (playerSelection === 'rock' && computerSelection === 'scissor') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissor' && computerSelection === 'paper')
     ) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        span.textContent = playerSelection;
+        div.insertBefore(span, spanVs)
     } else {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        span.textContent = playerSelection;
+        div.insertBefore(span, spanVs)
     }
 }
 
-function playGame() {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-}
+rockButton.addEventListener("click", () => playRound("rock"));
+paperButton.addEventListener("click", () => playRound("paper"));
+scissorButton.addEventListener("click", () => playRound("scissor"));
 
-
+const span = document.createElement("span");
+const spanC = document.createElement("span");
